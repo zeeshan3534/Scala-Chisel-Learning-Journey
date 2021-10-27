252376baccf0e4aab6c5ef077a88e4ee48a22609
pythonesta <61948628+zeeshan3534@users.noreply.github.com>
2021-10-27 12:19:29 -0700

    Add files via upload

---
+++ src/test/scala/lab1/SignedCounterTest.scala
@@ -0,0 +1,15 @@
+package lab1
+import org.scalatest._
+import chisel3._
+import chiseltest._
+
+class CountTest extends FreeSpec with ChiselScalatestTester{
+    "CounterTest test" in {
+        test(new Counter(3.U)){ c=>
+            c.clock.step(20)
+            
+         
+           
+        }
+    }
+}
