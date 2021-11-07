1173f46b43786cf04dfa52b69eb124901e49ec33
pythonesta <61948628+zeeshan3534@users.noreply.github.com>
2021-11-07 11:29:39 +0500

    Update Mux.scala

--- src/main/scala/lab2/Mux.scala
+++ src/main/scala/lab2/Mux.scala
@@ -1,15 +1,15 @@
-package lab2
-import org.scalatest._
-import chisel3._
-import chiseltest._
-
-class CountTest extends FreeSpec with ChiselScalatestTester{
-    "c ounter_up_down test" in {
-        test(new counter_up_down(8)){c=>
-            c.clock.step(20)
-            
-         
-           
-        }
-    }
+package practice
+import chisel3 . _
+// Mux IO interface class
+class Mux_2to1_IO extends Bundle {
+val in_A = Input(UInt(32.W))
+val in_B = Input (UInt(32.W))
+val select = Input(Bool())
+val out = Output (UInt())
+}
+// 2 to 1 Mux implementation
+class Mux_2to1 extends Module {
+val io = IO (new Mux_2to1_IO)
+// update the output
+io . out := io.in_A & (~io.select) | io.in_B & io.select
 }
