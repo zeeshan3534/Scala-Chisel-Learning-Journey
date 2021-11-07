2a4e62b9fe8ed4e4cf1054d348ffcb11fba8f18f
pythonesta <61948628+zeeshan3534@users.noreply.github.com>
2021-11-07 11:17:11 +0500

    Update Encoder4to2Test.scala

--- src/test/scala/lab3/Encoder4to2Test.scala
+++ src/test/scala/lab3/Encoder4to2Test.scala
@@ -3,7 +3,7 @@ import org.scalatest._
 import chiseltest._
 import chisel3._
 
-class AndTest extends FreeSpec with ChiselScalatestTester{
+class Encoder4to2_Test extends FreeSpec with ChiselScalatestTester{
     "maja agya 2.0" in  {
         test (new Encoder4to2){ c =>
         c.io.in.poke("b0001".U )
