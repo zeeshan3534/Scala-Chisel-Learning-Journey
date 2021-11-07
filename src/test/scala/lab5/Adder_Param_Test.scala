2b9052bdd8129d5c77f43d470189e3e0db3da9fd
pythonesta <61948628+zeeshan3534@users.noreply.github.com>
2021-11-07 11:22:32 +0500

    Update Adder_Param_Test.scala

--- src/test/scala/lab5/Adder_Param_Test.scala
+++ src/test/scala/lab5/Adder_Param_Test.scala
@@ -3,7 +3,7 @@ import org.scalatest._
 import chiseltest._
 import chisel3._
 
-class AndTest extends FreeSpec with ChiselScalatestTester{
+class Adder_param_Test extends FreeSpec with ChiselScalatestTester{
     "Adder_param" in  {
             test(new Adder(32)){c=>
             c.io.in_0.poke(12.U)
