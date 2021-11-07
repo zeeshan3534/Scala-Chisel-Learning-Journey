24fc55a48757aa98065c732a12fc8c4f86376728
pythonesta <61948628+zeeshan3534@users.noreply.github.com>
2021-11-07 11:12:28 +0500

    Update SignedCounterTest.scala

--- src/test/scala/lab1/SignedCounterTest.scala
+++ src/test/scala/lab1/SignedCounterTest.scala
@@ -3,7 +3,7 @@ import org.scalatest._
 import chisel3._
 import chiseltest._
 
-class CountTest extends FreeSpec with ChiselScalatestTester{
+class CountTest1 extends FreeSpec with ChiselScalatestTester{
     "CounterTest test" in {
         test(new Counter(3.U)){ c=>
             c.clock.step(20)
