c5a0e3d32de2cccf36acf7d41b690c8277567351
pythonesta <61948628+zeeshan3534@users.noreply.github.com>
2021-11-07 11:49:20 +0500

    Update exc3TEST.scala

--- src/test/scala/lab5/exc3TEST.scala
+++ src/test/scala/lab5/exc3TEST.scala
@@ -5,7 +5,7 @@ import chiseltest._
 
 class Operatortest extends FreeSpec with ChiselScalatestTester{
     "Operator tEST" in {
-        test(new Operator(4,UInt(16.W))(+)){c=>
+        test(new Operator(4,UInt(16.W))(_+_)){c=>
         // c.io.in.poke(Vec(4.UInt))
         c.io.in(0).poke(1.U)
         c.io.in(1).poke(2.U)
