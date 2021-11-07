a49d4a126d527b5d29d2063b5759a94b03948c95
pythonesta <61948628+zeeshan3534@users.noreply.github.com>
2021-11-07 11:12:45 +0500

    Update UpDownCounterTest.scala

--- src/test/scala/lab1/UpDownCounterTest.scala
+++ src/test/scala/lab1/UpDownCounterTest.scala
@@ -3,7 +3,7 @@ import org.scalatest._
 import chisel3._
 import chiseltest._
 
-class CountTest extends FreeSpec with ChiselScalatestTester{
+class CountTest2 extends FreeSpec with ChiselScalatestTester{
     "c ounter_up_down test" in {
         test(new counter_up_down(8)){c=>
             c.clock.step(20)
